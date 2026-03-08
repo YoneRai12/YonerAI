e71ce2166538e15cff811a7c2bbb95dad6afd23a
YoneRai12 <m3220r7444@icloud.com>
2026-03-08 18:01:58 +0900

    fix: enforce requester voice checks in music pickers
    (cherry picked from commit fc6db091ed09c0f0ea18a37440aedc62c8b03da0)

--- src/views/music_picker.py
+++ src/views/music_picker.py
@@ -141,7 +141,7 @@ class MusicPickView(ui.View):
             # The cog method will verify the user is in VC.
             ctx = await self.cog.bot.get_context(self.message) if self.message else None
             if ctx:
-                await self.cog.play_from_ai(ctx, url)
+                await self.cog.play_from_ai(ctx, url, requester=interaction.user)
             else:
                 await interaction.followup.send(f"🎵 再生: {title}\n{url}")
         except Exception as e:
